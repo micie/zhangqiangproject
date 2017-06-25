eaf51c4a68d2208f569a038f845abd84909844e6
Bobo <admin@admin.com>
2017-06-25 23:47:51 +0800

    正式开始

---
+++ backend/views/question/index.php
@@ -0,0 +1,358 @@
+
+<?php
+use yii\widgets\LinkPager;
+use yii\bootstrap\ActiveForm;
+use backend\models\Question;
+use yii\helpers\Url;
+
+$modelLabel = new \backend\models\Question();
+?>
+
+<?php $this->beginBlock('header');  ?>
+<!-- <head></head>中代码块 -->
+<?php $this->endBlock(); ?>
+
+<!-- Main content -->
+<section class="content">
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="box">
+      
+        <div class="box-header">
+          <h3 class="box-title">问答管理</h3>
+          <div class="box-tools">
+            <div class="input-group input-group-sm" style="width: 150px;">
+                
+        			
+        		<!-- <button id="delete_btn" type="button" class="btn btn-xs btn-danger">批量删除</button> -->
+            </div>
+          </div>
+        </div>
+        <!-- /.box-header -->
+        
+        <div class="box-body">
+          <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+            <!-- row start search-->
+          	<div class="row">
+          	<div class="col-sm-12">
+                <?php ActiveForm::begin(['id' => 'Question-search-form', 'method'=>'get', 'options' => ['class' => 'form-inline'], 'action'=>Url::toRoute('Question/index')]); ?>     
+                
+                  <div class="form-group" style="margin: 5px;">
+                      <label><?=$modelLabel->getAttributeLabel('id')?>:</label>
+                      <input type="text" class="form-control" id="query[id]" name="query[id]"  value="<?=isset($query["id"]) ? $query["id"] : "" ?>">
+                  </div>
+              <div class="form-group">
+              	<a onclick="searchAction()" class="btn btn-primary btn-sm" href="#"> <i class="glyphicon glyphicon-zoom-in icon-white"></i>搜索</a> | <button id="create_btn" type="button" class="btn btn-sm btn-primary">添加</button>
+           	  </div>
+               <?php ActiveForm::end(); ?> 
+            </div>
+          	</div>
+          	<!-- row end search -->
+          	
+          	<!-- row start -->
+          	<div class="row">
+          	<div class="col-sm-12">
+          	<table id="data_table" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="data_table_info">
+            <thead>
+            <tr role="row">
+            
+            <?php 
+		      echo '<th><input id="data_table_check" type="checkbox"></th>';
+              echo '<th class="sorting" tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('id').'</th>';
+              echo '<th class="sorting" tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('question').'</th>';
+              echo '<th class="sorting" tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('answer').'</th>';
+              echo '<th class="sorting" tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('sort_order').'</th>';
+         
+			?>
+	
+            <th tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >操作</th>
+            </tr>
+            </thead>
+            <tbody>
+            
+            <?php
+            $row = 0;
+            foreach ($models as $model) {
+                echo '<tr id="rowid_' . $model->id . '">';
+                echo '  <td><label><input type="checkbox" value="' . $model->id . '"></label></td>';
+                echo '  <td>' . $model->id . '</td>';
+                echo '  <td>' . mb_substr($model->question,0,20,'utf-8') . '...</td>';
+                echo '  <td>' . mb_substr($model->answer,0,20,'utf-8') . '...</td>';
+                echo '  <td>' . $model->sort_order . '</td>';
+                echo '  <td class="center">';
+                echo '      <a id="view_btn" onclick="viewAction(' . $model->id . ')" class="btn btn-primary btn-sm" href="#"> <i class="glyphicon glyphicon-zoom-in icon-white"></i>查看</a>';
+                 echo '      <a id="edit_btn" onclick="editAction(' . $model->id . ')" class="btn btn-primary btn-sm" href="#"> <i class="glyphicon glyphicon-edit icon-white"></i>修改</a>';
+                echo '      <a id="delete_btn" onclick="deleteAction(' . $model->id . ')" class="btn btn-danger btn-sm" href="#"> <i class="glyphicon glyphicon-trash icon-white"></i>删除</a>';
+                echo '  </td>';
+                echo '<tr/>';
+            }
+            
+            ?>
+            
+           
+           
+            </tbody>
+            <!-- <tfoot></tfoot> -->
+          </table>
+          </div>
+          </div>
+          <!-- row end -->
+          
+          <!-- row start -->
+          <div class="row">
+          	<div class="col-sm-5">
+            	<div class="dataTables_info" id="data_table_info" role="status" aria-live="polite">
+            		<div class="infos">
+            		从<?= $pages->getPage() * $pages->getPageSize() + 1 ?>            		到 <?= ($pageCount = ($pages->getPage() + 1) * $pages->getPageSize()) < $pages->totalCount ?  $pageCount : $pages->totalCount?>            		 共 <?= $pages->totalCount?> 条记录</div>
+            	</div>
+            </div>
+          	<div class="col-sm-7">
+              	<div class="dataTables_paginate paging_simple_numbers" id="data_table_paginate">
+              	<?= LinkPager::widget([
+              	    'pagination' => $pages,
+              	    'nextPageLabel' => '下一页',
+              	    'prevPageLabel' => '上一页',
+              	    'firstPageLabel' => '首页',
+              	    'lastPageLabel' => '尾页',
+              	]); ?>	
+              	
+              	</div>
+          	</div>
+		  </div>
+		  <!-- row end -->
+        </div>
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+    </div>
+    <!-- /.col -->
+  </div>
+  <!-- /.row -->
+</section>
+<!-- /.content -->
+
+<div class="modal fade" id="edit_dialog" tabindex="-1" role="dialog"
+	aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">×</button>
+				<h3>Settings</h3>
+			</div>
+			<div class="modal-body">
+                <?php $form = ActiveForm::begin(["id" => "question-form", "class"=>"form-horizontal", "action"=>Url::toRoute("question/save")]); ?>                      
+                 <input type="hidden" class="form-control" id="id" name="Question[id]" />
+                 <!-- 
+          <div id="id_div" class="form-group">
+              <label for="id" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("id")?></label>
+              <div class="col-sm-10">
+          
+              </div>
+              <div class="clearfix"></div>
+          </div>
+             -->
+          <div id="question_div" class="form-group">
+              <label for="question" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("question")?></label>
+              <div class="col-sm-10">
+                  <textarea class="form-control" id="question" name="Question[question]" placeholder="必填" > </textarea>
+              </div>
+              <div class="clearfix"></div>
+          </div>
+
+          <div id="answer_div" class="form-group">
+              <label for="answer" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("answer")?></label>
+              <div class="col-sm-10">
+                  <textarea class="form-control" id="answer" name="Question[answer]" placeholder="必填" > </textarea>
+              </div>
+              <div class="clearfix"></div>
+          <div id="sort_order_div" class="form-group">
+              <label for="sort_order" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("sort_order")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="sort_order" name="Question[sort_order]" placeholder="必填" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+
+          </div>
+
+			<?php ActiveForm::end(); ?>          
+                </div>
+			<div class="modal-footer">
+				<a href="#" class="btn btn-default" data-dismiss="modal">关闭</a> <a
+					id="edit_dialog_ok" href="#" class="btn btn-primary">确定</a>
+			</div>
+		</div>
+	</div>
+</div>
+<?php $this->beginBlock('footer');  ?>
+<!-- <body></body>后代码块 -->
+ <script>
+ function searchAction(){
+		$('#Question-search-form').submit();
+	}
+ function viewAction(id){
+		initModel(id, 'view', 'fun');
+	}
+
+ function initEditSystemModule(data, type){
+	if(type == 'create'){
+		$("#id").val('');
+		$("#question").val('');
+    $("#sort_order").val('');
+    $("#answer").val('');
+		
+	}
+	else{
+		$("#id").val(data.id);
+    	$("#question").val(data.question);
+      $("#answer").val(data.answer);
+      $("#sort_order").val(data.sort_order);
+    	}
+	if(type == "view"){
+      $("#id").attr({readonly:true,disabled:true});
+      $("#question").attr({readonly:true,disabled:true});
+      $("#sort_order").attr({readonly:true,disabled:true});
+      $("#answer").attr({readonly:true,disabled:true});
+	  $('#edit_dialog_ok').addClass('hidden');
+	}
+	else{
+      $("#id").attr({readonly:false,disabled:false});
+      $("#question").attr({readonly:false,disabled:false});
+      $("#sort_order").attr({readonly:false,disabled:false});
+      $("#answer").attr({readonly:false,disabled:false});
+      
+	  $('#edit_dialog_ok').removeClass('hidden');
+	}
+	$('#edit_dialog').modal('show');
+}
+
+function initModel(id, type, fun){
+	
+	$.ajax({
+		   type: "GET",
+		   url: "<?=Url::toRoute('question/view')?>",
+		   data: {"id":id},
+		   cache: false,
+		   dataType:"json",
+		   error: function (xmlHttpRequest, textStatus, errorThrown) {
+			    alert("出错了，" + textStatus);
+			},
+		   success: function(data){
+			   initEditSystemModule(data, type);
+		   }
+		});
+}
+	
+function editAction(id){
+	initModel(id, 'edit');
+}
+
+function deleteAction(id){
+	var ids = [];
+	if(!!id == true){
+		ids[0] = id;
+	}
+	else{
+		var checkboxs = $('#data_table :checked');
+	    if(checkboxs.size() > 0){
+	        var c = 0;
+	        for(i = 0; i < checkboxs.size(); i++){
+	            var id = checkboxs.eq(i).val();
+	            if(id != ""){
+	            	ids[c++] = id;
+	            }
+	        }
+	    }
+	}
+	if(ids.length > 0){
+		admin_tool.confirm('请确认是否删除', function(){
+		    $.ajax({
+				   type: "GET",
+				   url: "<?=Url::toRoute('question/delete')?>",
+				   data: {"ids":ids},
+				   cache: false,
+				   dataType:"json",
+				   error: function (xmlHttpRequest, textStatus, errorThrown) {
+					    alert("出错了，" + textStatus);
+					},
+				   success: function(data){
+					   for(i = 0; i < ids.length; i++){
+						   $('#rowid_' + ids[i]).remove();
+					   }
+					   admin_tool.alert('msg_info', '删除成功', 'success');
+					   window.location.reload();
+				   }
+				});
+		});
+	}
+	else{
+		admin_tool.alert('msg_info', '请先选择要删除的数据', 'warning');
+	}
+    
+}
+
+function getSelectedIdValues(formId)
+{
+	var value="";
+	$( formId + " :checked").each(function(i)
+	{
+		if(!this.checked)
+		{
+			return true;
+		}
+		value += this.value;
+		if(i != $("input[name='id']").size()-1)
+		{
+			value += ",";
+		}
+	 });
+	return value;
+}
+
+$('#edit_dialog_ok').click(function (e) {
+    e.preventDefault();
+	$('#question-form').submit();
+});
+
+$('#create_btn').click(function (e) {
+    e.preventDefault();
+    initEditSystemModule({}, 'create');
+});
+
+$('#delete_btn').click(function (e) {
+    e.preventDefault();
+    deleteAction('');
+});
+
+$('#question-form').bind('submit', function(e) {
+	e.preventDefault();
+	var id = $("#id").val();
+	var action = id == "" ? "<?=Url::toRoute('question/create')?>" : "<?=Url::toRoute('question/update')?>";
+    $(this).ajaxSubmit({
+    	type: "post",
+    	dataType:"json",
+    	url: action,
+    	data:{id:id},
+    	success: function(value) 
+    	{
+        	if(value.errno == 0){
+        		$('#edit_dialog').modal('hide');
+        		admin_tool.alert('msg_info', '添加成功', 'success');
+        		window.location.reload();
+        	}
+        	else{
+            	var json = value.data;
+        		for(var key in json){
+        			$('#' + key).attr({'data-placement':'bottom', 'data-content':json[key], 'data-toggle':'popover'}).addClass('popover-show').popover('show');
+        			
+        		}
+        	}
+
+    	}
+    });
+});
+
+ 
+</script>
+<?php $this->endBlock(); ?>
